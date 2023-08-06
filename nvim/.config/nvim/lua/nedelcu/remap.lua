324943289a1e68e8a1d3a21374f591191c828fa1
nedelcu-ioan <nedelcu.ioan.andrei@gmai.com>
2023-08-06 19:16:33 +0300

    feature: term + syntax highlighting

--- nvim/.config/nvim/lua/nedelcu/remap.lua
+++ nvim/.config/nvim/lua/nedelcu/remap.lua
@@ -1,4 +1,18 @@
 local builtin = require('telescope.builtin')
-vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 
+-- add options to function decorator
+function with_opts(fn, opts)
+    return function()
+        fn(opts)
+    end
+end
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>ff', with_opts(builtin.find_files, {hidden = true}), {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ex', vim.cmd.Lexplore)
+
+-- Open a terminal in vertical split
+vim.keymap.set('n', '<leader>tt', ':vnew term://bash<CR>')
+vim.keymap.set('n', '<leader>te', ':edit term://bash<CR>') 
+vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], termopts)
